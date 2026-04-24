b636b4a4bff6d7a70c4f929712af329bdf309bd0
George Essmat <georgeessmat89@gmail.com>
2026-04-24 22:25:41 +0300

    added thread_pool_init()

--- webserver_project/include/thread_pool.h
+++ webserver_project/include/thread_pool.h
@@ -1,6 +1,64 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
+#define INVALID_INPUT               1
+#define ERROR_INITIALIZING_QUEUE    2
+#define ERRNO_ERROR                 3
 
+#define THREAD_POOL_SIZE_STATIC     0
+#define THREAD_POOL_SIZE_DYNAMIC    1
+
+#define INITIAL_POOL_SIZE           10
+
+typedef enum{
+    FREE,
+    BUSY,
+    TERMINATED,
+    maxNumOfStates,
+}thread_state;
+
+typedef struct{
+    int max_size;
+    int curr_size;      //used for dynamic size option, otherwise is the same as max_size
+    int available_work;
+    int busy_threads_num;
+    const char* pool_name;
+    int size_option;
+    thread_state* threads_states;
+    pthread_t* tids;
+    fifo_queue_t* queue;
+    pthread_mutex_t* pool_lock;
+    sem_t* pool_sem;
+}thread_pool_t;
+
+typedef struct{
+    thread_pool_t* thrd_pl;
+    thread_state* state;
+    sem_t* init_sem;
+}thread_args;
+
+
+/**
+ * @brief initializes the thread pool
+ * @param thrd_pl will point to the initialized thread pool
+ * @param max_thread_num the maximum number of threads that the thread pool can have
+ * @param size_option can be:
+ *          THREAD_POOL_SIZE_STATIC: the pool is initialized with this size
+ *          THREAD_POOL_SIZE_DYNAMIC: the pool is initialized with minimum of max_thread_num and
+ *              INITIAL_POOL_SIZE and the sizes changes based on needs
+ * @param pool_name a null terminated character string of the pool name (used to initialize 
+ *                  unique semaphores)
+ * @return 0 if everything is okay or,
+ *         INVALID_INPUT: if any of the inputs have invalid value
+ *         ERROR_INITIALIZING_QUEUE: if the queue initialization fails
+ *         ERRNO_ERROR: check errno for the error
+ */
+int thread_pool_init(thread_pool_t* thrd_pl, int max_thread_num, int size_option, const char* pool_name);
+
+int add_work(thread_pool_t* thrd_pl, int client_sockfd);
+
+void* thread_task(void* arg);
+
+int thread_pool_destroy(thread_pool_t* thrd_pl, int option);
 
 #endif
--- webserver_project/src/network/thread_pool.c
+++ webserver_project/src/network/thread_pool.c
@@ -1,1 +1,98 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "../../include/fifo_queue.h"
 #include "../../include/thread_pool.h"
+
+
+int thread_pool_init(thread_pool_t* thrd_pl, int max_thread_num, int size_option, const char* pool_name)
+{
+    if((thrd_pl == NULL) || (max_thread_num < 1)
+        || (pool_name == NULL) || ((size_option != THREAD_POOL_SIZE_STATIC)
+        && (size_option != THREAD_POOL_SIZE_DYNAMIC)))
+                        return INVALID_INPUT;
+
+    thrd_pl = (thread_pool_t*)malloc(sizeof(thread_pool_t));
+    thrd_pl->max_size = max_thread_num;
+
+    if((thrd_pl->size_option = size_option) == THREAD_POOL_SIZE_DYNAMIC)
+    {
+        thrd_pl->curr_size = (max_thread_num < INITIAL_POOL_SIZE)?max_thread_num:INITIAL_POOL_SIZE;
+    }
+    else
+    {
+        thrd_pl->curr_size = max_thread_num;
+    }
+
+    thrd_pl->available_work = 0;
+    thrd_pl->busy_threads_num = 0;
+    thrd_pl->pool_name = pool_name;
+
+    if(fifo_queue_init(thrd_pl->queue) != 0)
+    {
+        free(thrd_pl);
+        return ERROR_INITIALIZING_QUEUE;
+    } 
+
+    if(pthread_mutex_init(thrd_pl->pool_lock, NULL) != 0)
+    {
+        fifo_queue_destroy(thrd_pl->queue);
+        free(thrd_pl);
+        return ERRNO_ERROR;
+    }
+
+    if((thrd_pl->pool_sem = sem_open(thrd_pl->pool_name, O_CREAT, 0666, 0)) != 0)
+    {
+        fifo_queue_destroy(thrd_pl->queue);
+        pthread_mutex_destroy(thrd_pl->pool_lock);
+        free(thrd_pl);
+        return ERRNO_ERROR;
+    }
+
+
+    thrd_pl->threads_states = (thread_state*)malloc(thrd_pl->curr_size*sizeof(thread_state));
+    thrd_pl->tids = (pthread_t*)malloc(thrd_pl->curr_size*sizeof(pthread_t));
+
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    //threads arguments
+    thread_args* args = (thread_args*)malloc(sizeof(thread_args));
+    args->thrd_pl = thrd_pl;
+
+    sem_t* pool_init_sem = sem_open("INIT_SEM", O_CREAT, 0666, 0);
+    args->init_sem = pool_init_sem;     //just for initializing the pool
+    for(int i = 0; i < thrd_pl->curr_size; i++)
+    {
+        thrd_pl->threads_states[i] = FREE;
+        args->state = &thrd_pl->threads_states[i];
+
+        pthread_create(&thrd_pl->tids[i], &attr, thread_task, (void*)args);
+
+        //wait for the just created thread to complete its initialization
+        sem_wait(pool_init_sem);
+    }
+
+
+    sem_unlink("INIT_SEM");
+    free(args);
+
+    return 0;
+}
+
+
+void* thread_task(void* args)
+{
+    thread_state* state = ((thread_args*)args)->state;
+    thread_pool_t* thrd_pl = ((thread_args*)args)->thrd_pl;
+
+    //let the thread pool continue initializing the other threads
+    sem_post(((thread_args*)args)->init_sem);
+
+    //do work
+
+}
