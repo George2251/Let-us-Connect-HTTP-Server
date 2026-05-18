33043716d7a8d73890719030f6fb0ad66ba5952b
3omd4 <mohamedemadeldin38@gmail.com>
2026-05-18 06:54:24 +0300

    updated george part until it revise it

--- webserver_project/include/fifo_queue.h
+++ webserver_project/include/fifo_queue.h
@@ -1,57 +1,27 @@
 #ifndef FIFO_QUEUE_H
 #define FIFO_QUEUE_H
 
+// Tell the queue that ClientTask exists
+struct ClientTask;
 
-#define INVALID_INPUT       1
-#define EMPTY_QUEUE         2
-#define NOT_EMPTY_QUEUE     3
+#define INVALID_INPUT -1
+#define EMPTY_QUEUE -2
 
-#include "network.h"
-
-typedef struct node_t
-{
+// The node now holds our struct ClientTask pointer
+typedef struct node {
     struct ClientTask* task;
-    struct node_t* next;
-    struct node_t* prev;
-}node_t;
+    struct node* next;
+    struct node* prev;
+} node_t;
 
-
-typedef struct
-{
+typedef struct {
     int size;
     node_t* first;
     node_t* last;
-}fifo_queue_t;
+} fifo_queue_t;
 
-
-/**
- * @brief Initiates a fifo queue to be used by the threads
- * @param queue will be assigned the address of the initialized fifo_queue_t
- * @return 0: everything is okay,
- */
 int fifo_queue_init(fifo_queue_t* queue);
-
-/**
- * @brief add a node to the tail of the queue
- * @param queue will have a node added to its tail
- * @param task will be added to the queue
- * @return 0: everything is okay, 
- *         INVALID_INPUT: invalid value for @p queue
- * @details must be locked before use so only one thread can update @p queue
- */
 int push_last(fifo_queue_t* queue, struct ClientTask* task);
-
-/**
- * @brief pops a node from the head of the queue
- * @param queue will have the head poped
- * @param task will be filled with from the queue
- * @return 0: everything is okay,
- *         INVALID_INPUT: invalid value for @p queue or @p sockfd
- *         EMPTY_QUEUE: @p queue is empty
- * @details must be locked before use so only one thread can update @p queue         
- */
 int pop_first(fifo_queue_t* queue, struct ClientTask** task);
 
-
-
-#endif
+#endif // FIFO_QUEUE_H
